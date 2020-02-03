a07bc57760102aed03bdf637557a43871c2eac61
藤野貴明 <fujinotakaaki@fujinotakaakinoMacBook-Pro.local>
2020-02-03 16:29:07 +0900

    fix routing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
 
   def hello
     arr = [('a'..'z'), ('A'..'Z'),('0'..'9')].map(&:to_a).flatten
-    render html:"Hello World" + (0..9).map{arr[rand(62)]}.join
+    render html:"Hello World!! " + (0..9).map{arr[rand(62)]}.join
   end
 end
