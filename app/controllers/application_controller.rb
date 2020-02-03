57e7539d919268f9fb81bd0ccc83eca9a82811c8
藤野貴明 <fujinotakaaki@fujinotakaakinoMacBook-Pro.local>
2020-02-03 19:26:05 +0900

    Finish static pages about §3.5 of RoR-Tutorial

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,3 @@
 class ApplicationController < ActionController::Base
 
-  def hello
-    arr = [('a'..'z'), ('A'..'Z'),('0'..'9')].map(&:to_a).flatten
-    render html:"Hello World!! " + (0..9).map{arr[rand(62)]}.join
-  end
 end
