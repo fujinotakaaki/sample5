9d812c6804c1489049dabd2f74e7994be3a5b237
藤野貴明 <fujinotakaaki@fujinotakaakinoMacBook-Pro.local>
2020-02-03 16:13:57 +0900

    Fix program to be able to use heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+
+  def hello
+    arr = [('a'..'z'), ('A'..'Z'),('0'..'9')].map(&:to_a).flatten
+    render html:"Hello World" + (0..9).map{arr[rand(62)]}.join
+  end
 end
