5625c9176afefa8f97538ba361d1fc90e7c4a337
藤野貴明 <fujinotakaaki@fujinotakaakinoMacBook-Pro.local>
2020-02-08 15:25:34 +0900

    before 6.3. Install varidation to email about unique etc...

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,21 @@
 class User < ApplicationRecord
+  # 保存前にemail属性を小文字に変換してメールアドレスの一意性を保証する
+  before_save { self.email = self.email.downcase }
+  # before_save { self.email.downcase! } #こっちも可（同義）
+  # ちなみに、左式ではself.を省略することはできません。
+
   # たぶんvarlidates関数の記述は下記のはず
   # def validates( :symbol, **changes )
   # def validates( :symbol, presense:false, uniq:true,.... )
   # :symbolが存在しないとエラーになる感じかな？
   # :presence => trueはString#blank?が聞いてると思われる
-  validates(:name, :presence => true, :length => { :in => (1..50) } )
+  validates( :name, :presence => true, :length => { :in => (1..50) } )
   # validates :name,  presence: true, length: { in: (1..50) }
   # format:{with:Regexp}で正規表現に従うもののみを通すことができるようになる。
   # VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates(:email, :presence => true, :length => { :in => (3...256) }, :format => { :with => /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, :uniqueness => { :case_sensitive => false } )
+  validates( :email, :presence => true, :length => { :in => (3...256) },
+  :format => { :with => /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+  :uniqueness => { :case_sensitive => false } )
   # uniqueness:trueでは大文字小文字は区別される
   # 大文字と小文字の区別をなくす場合はuniqueness:true =>uniqueness: { case_sensitive: false }に置き換えることで実現できる（先輩優しいっすね）
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -51,7 +51,7 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "email validation should reject invalid addresses" do
-    # 向こうなメールアドレスはちゃんと振り落とされるか検証
+    # 無効なメールアドレスはちゃんと振り落とされるか検証
     invalid_addresses = %w(user@example,com user_at_foo.org user.name@example. foo@bar_baz.com foo@bar+baz.com )
     invalid_addresses.each do |invalid_address|
       @user.email = invalid_address
@@ -64,5 +64,40 @@ class UserTest < ActiveSupport::TestCase
     duplicate_user = User.new( name:@user.name, email:@user.email.upcase )
     @user.save
     assert_not duplicate_user.valid?
+    # ここら辺（重複関係）のテストでエラーになるなら、db/migrate/(date)_index_to_add_...#!/usr/bin/env ruby -wKU
+    # changeメソッドのadd_index実装してないのか？
+    # それとも、test/fixtures/users.ymlの全文をコメントアウトしてないとか？
+  end
+
+  test "email addresses should be saved as lower-case" do
+    # app/models/user.rbでbefore_save { self.email = email.downcase }を実装しているため、
+    # 元の入力と登録されているメールアドレスは異なる。（大文字が１個もないなら話は別）
+    mixed_case_email = "FooBAr@ExAMPle.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+    # assert_equal mixed_case_email.downcase, @user.email
+    # before_save { self.email.downcase! }にすると、↑のように#reload必要なしにできるらしい
+    # ちなみに、これにしなくてもテスト通っちゃったんですがそれは...（困惑）
+    # じゃけん、reloadメソッド使う方にしましょうね
+    # ------解説-----------------------------------------------------------------------------------------
+    # Biim兄貴「最後コードは、元のemailアドレス（左辺）とDB上のemailアドレスが一致しているか比較しています。」
+    # Biim兄貴「メモリ（ここでは@userに限定）に保存されているものとDBに保存されているデータは」
+    # Biim兄貴「"常に"一致しているとは限りません（なんで？（殺意））」
+    # Biim兄貴「だから、リロード（リフレッシュ・最新の状態に）してやる必要があったんですね。」
+
+    # Biim兄貴「まあ、@userはDBの中身を参照しているわけじゃないし（多分メモリを参照している）、」
+    # Biim兄貴「DBと@userの中身が違っちゃうのは仕方ないね♂」
+    # -----------------------------------------------------------------------------------------------------
+    # ~~~reloadメソッドについて~~~
+    # app配下(あたり)にあるソースコードを読み直します。（ざっくり）
+    # ↓多分こんな感じらしい↓
+    # $ rails console --sandbox
+    # @sample = Sample.find(1) # @sample.name => 'Tadokoro'
+    # Sample.find(1).update_column(:name, 'Yajue')
+    # @sample.name # =>  # @sample.name => 'Tadokoro'
+    # @sample.reload.name => # @sample.name => 'Yajue'
+    # reference: https://teratail.com/questions/2230
+    # -----------------------------------------------------------------------------------------------------
   end
 end # class UserTest < ActiveSupport::TestCase
